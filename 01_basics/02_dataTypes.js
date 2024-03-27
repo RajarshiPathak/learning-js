c23df67fc72350c5163eef92d471e198162c5d9f
Rajarshi Pathak <165082897+RajarshiPathak@users.noreply.github.com>
2024-03-27 09:55:43 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -4,3 +4,22 @@
 
 console.log(3+3);
 
+console.log("Hitesh");
+
+let name = "rajarshi"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power53
+//bigint
+//string =>"   "
+//boolean => true/false
+//null=> standalone value
+// undefined =>
+// symbol =>unique
+
+//object 
+
+console.log(typeof null) // object 
+console.log(typeof undefined) //undefined
