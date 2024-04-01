83fbca80526b38891be271edd19016bb951f0038
Rajarshi Pathak <165082897+RajarshiPathak@users.noreply.github.com>
2024-04-01 17:10:47 +0000

    tried conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+
+let isloggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber) 
+//type string hi rahega 
+
+//******************************Operations*************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Rajarshi"
+
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1"+2);
+// //string + num = string
+// console.log(1 + "2");
+// // jab do operand honge to concatination
+// console.log("1"+ 2 +2)
+// //operation depends on the type of first operand
+// console.log(1 + 2 + "2")
+// // jaise isme plus hua pehle 
+
+// console.log((3+4)*5%3)
+// //precedense of modulus % is less tha *multiple
+
+// console.log(+true)
+// console.log(""+"")
+
+let num1 ,num2, num3
+
+num1 = num2 = num3 = 2+2
+console.log(num2,num3)
+
+let gameCountr = 100
+++gameCountr
+console.log(gameCountr);
+
+
+
+
+
+
+
+
