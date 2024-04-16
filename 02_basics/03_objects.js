da09cfa8651aa6355aff5de1d091fc5b84457052
Rajarshi Pathak <165082897+RajarshiPathak@users.noreply.github.com>
2024-04-16 14:30:14 +0000

    objects part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+//singleton
+//isse banta Object.create
+
+//object literals
+//ye ek bhi singleton nahi hote
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name:"Hitesh",
+    "full name":"Hitesh Choudhary",
+    [mySym]:"mykey1",
+    age:18,
+
+    location:"Jaipur",
+    email:"hitesh@google.com",
+    isLoggedIn:false,
+    latLoginDays:["Monday","Saturday"]
+
+}
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "hitesh@chatgpt.com"
+//Object.freeze(JsUser)
+//ye value fix kar deta hai 
+JsUser.email = "hitesh@microsoft.com"
+//console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello JS user")
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user,${this.name}`)
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
