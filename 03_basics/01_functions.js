538ac253901459a2bd7b5df9ced302535a2d080d
Rajarshi Pathak <165082897+RajarshiPathak@users.noreply.github.com>
2024-04-19 18:15:34 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,13 +36,37 @@ function loginUserMessage(username = "sam"){
 }
 
 //console.log(loginUserMessage("hitesh"));
-console.log(loginUserMessage());
+//console.log(loginUserMessage());
 //yaha sam as a default value ayega 
 
 function calculateCartPrice(val1,val2, ...num1){
     return num1
 }
 
+//console.log(calculateCartPrice(200,400,500,2000));
 
+const user = {
+    username: "hitesh",
+    price:199
+}
 
+function  handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
 
+}
+// handleObject(user)
+//above is the one way to pass object to function
+handleObject({
+    username:"sam",
+    price:399
+})
+//this is another way to pass object 
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+//or we can directly pass the array
+
+console.log(returnSecondValue([200,400,500,1000]))
