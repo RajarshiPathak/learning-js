7ed322cf32d68362f40c3e55b3bbe5dac825d8e0
Rajarshi Pathak <165082897+RajarshiPathak@users.noreply.github.com>
2024-04-22 13:25:10 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("mar");
+        break;
+    case "april":
+        console.log("apr");
+        break;
+
+    default:
+        console.log("default case")
+        break;
+}
